4dca6a692e8a3a1c935e23b940b601309c22a27b
lilylay19 <laylily319@gmail.com>
2026-08-26 16:15:31 +0700

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Navbar from "./Components/Nav/Navbar";
 import "./App.css";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Footer/>
     </>
+    
   );
 }
 
