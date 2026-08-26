005e258d0ef9bc0a20751c37974bdefd10c8393c
Hok Chheng <hokchheng019@gmail.com>
2026-08-26 13:54:52 +0700

    Added : NavBar Component, Font, color variables

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "./index.css";
-import App from "./App.jsx";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import "./index.css";
+import App from "./App.jsx";
+import { store } from "./Components/redux/store";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <App />,
   },
 ]);
 
 const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
-  <Provider>
-    <RouterProvider router={router} />
-  </Provider>
-)
+  <StrictMode>
+    <Provider store={store()}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
