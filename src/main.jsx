b88718fff5fcd509ed5e99eef9d25c49e6119d02
Nin Bora <ninbora780@gmail.com>
2026-08-29 21:37:06 +0700

    change main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
-
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
 import "./index.css";
 import App from "./App.jsx";
+
 import { store } from "./Components/redux/store";
-import { createRoot } from 'react-dom/client'
-import "./index.css";
 import { Provider } from "react-redux";
+
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import { store } from "./Components/redux/store.js";
 
 const router = createBrowserRouter([
   {
@@ -22,15 +17,11 @@ const router = createBrowserRouter([
 ]);
 
 const root = document.getElementById("root");
+
 ReactDOM.createRoot(root).render(
   <StrictMode>
-    <Provider store={store()}>
+    <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
-createRoot(root).render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
-)
