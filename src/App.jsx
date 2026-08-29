0452f74a657427edd092483c0d20bc00ce682d7a
Nin Bora <ninbora780@gmail.com>
2026-08-29 21:18:43 +0700

    cv sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./Components/Nav/Navbar";
 import "./App.css";
+import CvTemplate from "./Components/Pages/cvtemplate";
 
 function App() {
   return (
     <>
       <Navbar />
+      <CvTemplate />
     </>
   );
 }
